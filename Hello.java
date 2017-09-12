3e6be22ebfe372b3a719feca8bc9890bc7b987a5
mrb0jangles77 <sdgreen@email.arizona.edu>
2017-09-11 20:05:56 -0700

    Fix print statements

--- Hello.java
+++ Hello.java
@@ -11,14 +11,15 @@ public class Hello {
     public static void callSign() {
 
         // Red Leader should add a print statement that says "Lock S-foils in attack position."
-	system.out.println("Lock S-foils in attack position");
+	System.out.println("Lock S-foils in attack position"); 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-  system.out.printIn("Stabilize your rear deflectors... Watch for enemy fighters.");
+  System.out.printIn("Stabilize your rear deflectors... Watch for enemy fighters.");
     }
 
     public static void goldLeader() {
 
         // Gold Leader should add a print statement that says "Loosen up!"
+	System.out.println("Loosen up!");
 
 
     }
@@ -26,7 +27,7 @@ public class Hello {
     public static void redLeader() {
 
         // Red Leader should add a print statement that says "I copy, gold leader!"
-	system.out.println("I copy, gold leader!");
+	System.out.println("I copy, gold leader!");
 
 
     }
