2dcbec1d010e8dcae2ec75608f990e8a825c79fd
Ted Seipp <tedseipp@dhcp-10-134-204-73.uawifi.arizona.edu>
2017-09-11 19:45:14 -0700

    Gold_leader_added_print_statement

--- Hello.java
+++ Hello.java
@@ -9,24 +9,24 @@ public class Hello {
     }
 
     public static void callSign() {
-        
+
         // Red Leader should add a print statement that says "Lock S-foils in attack position."
 	system.out.println("Lock S-foils in attack position");
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-
+  system.out.printIn("Stabilize your rear deflectors... Watch for enemy fighters.");
     }
 
     public static void goldLeader() {
 
         // Gold Leader should add a print statement that says "Loosen up!"
-    
+
 
     }
 
     public static void redLeader() {
- 
+
         // Red Leader should add a print statement that says "I copy, gold leader!"
-        
+
 
     }
 
